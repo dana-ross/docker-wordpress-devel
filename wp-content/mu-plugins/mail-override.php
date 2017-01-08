518d777b0702ec5ab1f74ebc6dbfbdc76eb33041
Dave Ross <dave@davidmichaelross.com>
2017-01-08 00:59:03 -0500

    Added comments to mail-override.php mu-plugin

--- wp-content/mu-plugins/mail-override.php
+++ wp-content/mu-plugins/mail-override.php
@@ -1,3 +1,13 @@
 <?php
 
+/**
+ * Custom mail-related functions to fix mail sending
+ * when the site is just known as "localhost"
+ * @see https://github.com/daveross/docker-wordpress-devel
+ */
+
+/**
+ * Override the mail sending address to wordpress@example.com
+ * @see https://tools.ietf.org/html/rfc2606
+ */
 add_filter( 'wp_mail_from', function() { return 'wordpress@example.com'; } );
